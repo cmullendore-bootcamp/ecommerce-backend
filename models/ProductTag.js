acb8c5c2a537efe269c090e210e01543a1cd46f3
Chris Mullendore <chris@vindage.com>
2022-05-25 04:55:29 -0700

    Implemented objects and routes

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -4,17 +4,25 @@ const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
-ProductTag.init(
-  {
-    // define columns
-  },
-  {
+ProductTag.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrementIdentity: true
+    },
+    product_tag: {
+        type: DataTypes.INTEGER
+    },
+    tag_id: {
+        type: DataTypes.INTEGER
+    }
+}, {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'product_tag',
-  }
-);
+});
 
 module.exports = ProductTag;
